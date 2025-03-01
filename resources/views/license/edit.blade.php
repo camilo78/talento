af084b4c66fd75b50db8abeb9f3beff0e7417f1c
camilo78 <camilo.alvarado0501@gmail.com>
2025-02-28 22:48:46 -0600

    fix: corregir la inclusión del script html2canvas en la vista de edición de licencia

--- resources/views/license/edit.blade.php
+++ resources/views/license/edit.blade.php
@@ -522,7 +522,7 @@
     </script>
 @endpush
 @push('print')
- <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
